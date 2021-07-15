d81cf82d05387f15d186b3e7c216a6c60bdf6faf
CharlieBGood <carlosbueno1196@gmail.com>
2021-07-15 17:58:57 -0500

    feat: users list in new contact

--- frontend/chatting-app/src/components/modals/NewContactComponent.jsx
+++ frontend/chatting-app/src/components/modals/NewContactComponent.jsx
@@ -1,86 +1,74 @@
 import { render } from '@testing-library/react'
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function NewContact() {
 
-    function RightSide(){
-        if(true){
-            return(
-                <img src="images/iconoFoto.png" className="img-fluid" id="fotoGrupo" alt="logo" />
-            );
+function RenderUsersList({users}){
+    var id = -1;
+    const usersList = users.sort().map((user) =>{
+        id++;
+        return(
+            <li className="list-group-item" id={"id_user_"+id}>
+                {user}
+            </li>
+        );
+    });
+
+    return(
+        <div class="scrollable w-100">
+            <ul className="list-group">
+                {usersList}
+            </ul>
+        </div>
+    );
+}
+
+function RightSide(){
+    if(true){
+        return(
+            <img src="images/iconoFoto.png" className="img-fluid" id="fotoGrupo" alt="logo" />
+        );
+    }
+    else{
+        return(
+            'hi there'
+        );
+    }
+};
+
+
+class NewContact extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+          users: ['Adele', 'Agnes', 'Paquita la del barrio', 'Billy Joel', 'Bob Dylan']
         }
-        else{
-            return(
-                'hi there'
-            );
-        }
-    };
+    }
 
-    return (
-        <div className='container'>
-            <div class="row">
-                {/* Parte izquierda: barra de búsqueda de contactos */}
-                <div className="col-5 text-center mt-5">
-
-                    <h2>Agregar al grupo</h2>
-                    <div className="form-group my-5">
-                        <input type="text" id="myInput" onkeyup="myFunction()" 
-                        placeholder="Nombre del contacto" title="Type in a name" 
-                        class="form-control w-75"/>
+    render(){
+        return (
+            <div className='container'>
+                <div class="row">
+                    {/* Parte izquierda: barra de búsqueda de contactos */}
+                    <div className="col-5 text-center mt-5">
+    
+                        <h1>App Users</h1>
+                        <div className="form-group my-5">
+                            <input type="text" id="myInput" onkeyup="myFunction()" 
+                            placeholder="Contact name..." title="Type in a name" 
+                            class="form-control w-75"/>
+                        </div>
+                        <RenderUsersList users={this.state.users} />
                     </div>
-                    
-
-                    <ul id="myUL" className="list-group">
-                        <li className="list-group-item active">
-                            <div class='form-check'>
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                            <label class="form-check-label" for="defaultCheck1">Adele</label>
-                            </div>
-                        </li>
-                        <li className="list-group-item">
-                            <div class='form-check'>
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                            <label class="form-check-label" for="defaultCheck1">Agnes</label>
-                            </div>
-                        </li>
-                        <li className="list-group-item">
-                            <div class='form-check'>
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                            <label class="form-check-label" for="defaultCheck1">Billy</label>
-                            </div>
-                        </li>
-                        <li className="list-group-item">
-                            <div class='form-check'>
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                            <label class="form-check-label" for="defaultCheck1">Bob</label>
-                            </div>
-                        </li>
-                        <li className="list-group-item">
-                            <div class='form-check'>
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                            <label class="form-check-label" for="defaultCheck1">Calvin</label>
-                            </div>
-                        </li>
-                        <li className="list-group-item">
-                            <div class='form-check'>
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                            <label class="form-check-label" for="defaultCheck1">Christina</label>
-                            </div>
-                        </li>
-                        <li className="list-group-item">
-                            <div class='form-check'>
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-                            <label class="form-check-label" for="defaultCheck1">Cindy</label>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-        
-                {/* Parte derecha: Configuraciones de grupo    */}
-                <div  class="col-7 text-center"> 
-                   <RightSide />
+            
+                    {/* Parte derecha: Configuraciones de grupo    */}
+                    <div  class="col-7 text-center"> 
+                       <RightSide />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default NewContact;
