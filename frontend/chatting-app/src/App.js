a068cee80cee90350cddcf8f9fd8e5c07567e711
CharlieBGood <carlosbueno1196@gmail.com>
2021-07-15 18:05:56 -0500

    refactor: develop folders refactor

--- frontend/chatting-app/src/App.js
+++ frontend/chatting-app/src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/MainComponent'
+import './App.css';
+import './Utils.css'
 
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+  render() {
+    return (
+      <BrowserRouter>
+          <Main />
+      </BrowserRouter>  
+    );
+  }  
+
 }
 
 export default App;
