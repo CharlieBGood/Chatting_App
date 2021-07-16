9a5b6b8766dc41990f962ef23b4cf04aaca57c4e
CharlieBGood <carlosbueno1196@gmail.com>
2021-07-16 12:01:02 -0500

    refactor: up to date with develop

--- frontend/chatting-app/src/App.js
+++ frontend/chatting-app/src/App.js
@@ -4,9 +4,6 @@ import Main from './components/MainComponent';
 import NewContact from "./components/modals/NewContact/NewContactComponent";
 import './App.css';
 import './Utils.css'
-import ChatsGrid from './components/chat/ChatsGrid'
-import UserProfile from "./components/profile/UserProfile";
-import Grid from '@material-ui/core/grid'
 
 
 class App extends Component{
