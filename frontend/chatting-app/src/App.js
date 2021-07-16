5adf846ac3996adba410535a1c42e3964318355b
CharlieBGood <carlosbueno1196@gmail.com>
2021-07-16 15:37:51 -0500

    feat: new icon and name of app

--- frontend/chatting-app/src/App.js
+++ frontend/chatting-app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import NewContact from "./components/modals/NewContact/NewContactComponent";
+import Configuration from "./components/Configuration"
+import ChatsGrid from './components/chat/ChatsGrid'
 import './App.css';
 import './Utils.css'
 
