1f150275d03d9445cd3190019043de6a9d20a3de
CharlieBGood <carlosbueno1196@gmail.com>
2021-07-16 20:28:24 -0500

    feat: miniature profile summary

--- frontend/chatting-app/src/components/ChatAppComponent.jsx
+++ frontend/chatting-app/src/components/ChatAppComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ChatsGrid from './chat/ChatsGrid'
 import ChatListComponent from "./chats_list/ChatsListComponent";
+import MiniatureProfileSummary from "./MiniatureProfileSummaryComponent";
 
 class ChatAppComponent extends Component {
     constructor(props){
@@ -12,7 +13,7 @@ class ChatAppComponent extends Component {
             <div class="row app-container">
                 <div class="col-3">
                     <div class="miniature-profile-summary">
-                        hi
+                        <MiniatureProfileSummary />
                     </div>
                     <div class="border">
                         <ChatListComponent />
